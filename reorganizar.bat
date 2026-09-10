0f9f3d8569050b302cab48606ea266f5dc726dba
matlarr0823 <lauty9888@gmail.com>
2026-09-09 23:47:46 -0300

    Organizo el repo en carpetas: ejercicios y proyecto-escaner

---
+++ reorganizar.bat
@@ -0,0 +1,49 @@
+@echo off
+cd /d "D:\github\pythonSecurity"
+
+echo Creando carpetas...
+mkdir ejercicios
+mkdir proyecto-escaner
+
+echo Moviendo ejercicios...
+move escero.py ejercicios\
+move eschico.py ejercicios\
+move FuncionCalcularDescuento ejercicios\
+move FuncionDevuelveValor.py ejercicios\
+move FuncionEsPar.py ejercicios\
+move FuncionParametroConsigna.py ejercicios\
+move FuncionSumarListas.py ejercicios\
+move inscriptocurso.py ejercicios\
+move intentos_numero7.py ejercicios\
+move inventarioviideojuego.py ejercicios\
+move jubilado.py ejercicios\
+move ListaCorreos.py ejercicios\
+move listainvitados.py ejercicios\
+move listavidasjugadores.py ejercicios\
+move mayorEdad.py ejercicios\
+move notaExamen.py ejercicios\
+move notaExcelente.py ejercicios\
+move notaMuybueno.py ejercicios\
+move numeroGrande.py ejercicios\
+move numeroPositivo.py ejercicios\
+move numerosPares.py ejercicios\
+move password.py ejercicios\
+move productosdisponibles.py ejercicios\
+move semaforo.py ejercicios\
+move temperatura.py ejercicios\
+move temperaturalista.py ejercicios\
+move usuarios.py ejercicios\
+move usuariospermitidos.py ejercicios\
+move while.py ejercicios\
+move whileholaalumno..py ejercicios\
+move whilepass.py ejercicios\
+move whilesumar100.py ejercicios\
+move EnunciadosEjercicios ejercicios\
+
+echo Moviendo proyecto del escaner...
+move escaneadorPuertos.py proyecto-escaner\
+
+echo.
+echo Listo! Los archivos quedaron organizados en las carpetas "ejercicios" y "proyecto-escaner".
+echo El README.md se queda en la raiz del repo.
+pause
